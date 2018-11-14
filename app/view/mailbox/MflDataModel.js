f98fe5cba7f04cd7024802b3ed1f6dd11303fc8d
Sean Roshan <sean.roshan.91@gmail.com>
2018-11-13 19:20:03 -0800

    modified folder tree datamodel

--- app/view/mailbox/MflDataModel.js
+++ app/view/mailbox/MflDataModel.js
@@ -1,26 +1,22 @@
+Ext.Ajax.cors = true;
+
 Ext.define("TutorialApp.view.mailbox.MflDataModel", {
   extend: "Ext.data.TreeModel",
-  fields: [
-    {
-      name: "recordid",
-      type: "string"
-    },
-    {
-      name: "foldername",
-      type: "string"
-    },
-    {
-      name: "foldertype",
-      type: "int"
-    },
-    {
-      name: "system",
-      type: "int"
-    }
-  ],
-  idProperty: "recordid",
   proxy: {
+    //type: "ajax",
+    //url: "../../../resources/data/data1.json"
     type: "ajax",
-    url: "../../../resources/data/data1.json"
+    url:
+      "https://morning-lowlands-10589.herokuapp.com/api/emailclient/ajax/5bc5581f2fe1400f3d447182/email_accounts/0/folders",
+    reader: "json",
+    method: "GET",
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    success: function(response) {
+      console.log(response);
+      //console.log(response.status + ' ' + response.statusText);
+      //console.dir(Ext.decode(response.responseText));
+    }
   }
 });
